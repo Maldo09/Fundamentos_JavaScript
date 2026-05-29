af9ae4445c874711f28ec8281fbbab63d4351b9c
Your Name <you@example.com>
2026-05-28 20:12:44 -0500

    subiendo

---
+++ Cap1.Introduccion/Ex01/ejercicio1.js
@@ -0,0 +1,9 @@
+/* 
+Esto sirve para realizar comentarios en el código, es decir, para escribir texto que no se ejecutara como código.
+Los comentarios son utiles para explicar el proposito de ciertas partes del código, para dejar notas para otros desarrolladores o para recordar algo importante sobre el codigo en el futuro. 
+*/
+
+//Esto es un comentario de una sola línea, se utiliza para escribir comentarios breves o para comentar una sola línea de código.
+
+document.write("Hola Mundo"); 
+console.log("Hola Mundo");
